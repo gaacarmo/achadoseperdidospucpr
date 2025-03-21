da10a120f91b07e9b960c7e758536c7183495107
IgorThiagoLara <163324764+IgorThiagoLara@users.noreply.github.com>
2025-03-21 11:35:51 -0300

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,8 @@
+create table Cadastro_usuario(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    nome_usuario VARCHAR(20),
+    senha VARCHAR(15),
+    email VARCHAR(100),
+    curso_usuario VARCHAR(50)
+);
